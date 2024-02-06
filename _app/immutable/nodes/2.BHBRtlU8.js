44dfe4f10e7f61fc07b43fb620a81c70ab0679a6
Hiemswolf <claytonkruse08@gmail.com>
2024-02-06 00:38:24 -0600

    first build

---
+++ _app/immutable/nodes/2.BHBRtlU8.js
@@ -0,0 +1,48 @@
+import{s as mt,n as R}from"../chunks/scheduler.kAlY8CUs.js";import{S as ct,i as ft,e as a,s as o,c as l,g as s,a as r,b as wt,d as i,f as n}from"../chunks/index.PAoPxtjW.js";function vt(F){let h,K="Clayton Kruse",S,d,G=`Hello, I am a full time CS student at Mizzou, who skipped CS 1050 for CS 2050
+	in my first year, and I am looking for summer internship opportunities.`,_,u,V="Contact Me",P,p,X='<tr><td>Email</td> <td><a href="mailto:claytonkruse08@gmail.com">claytonkruse08@gmail.com</a></td></tr> <tr><td>Phone</td> <td><a href="tel:+13147452624">(314) 745-2624</a></td></tr>',T,m,Y="Education",j,c,Z="In high school I took a number of classes related to computer science.",D,f,tt=`<li>Classes Taken in High School
+		<ul><li>Intro to Programming</li> <li>AP Computer Science
+				<ul><li>OOP &amp; Inheritence</li></ul></li> <li>Web Developement
+				<ul><li>Learned PHP</li> <li>Learned SQL with <a href="https://www.mysql.com/">MySQL</a></li></ul></li> <li>Networking
+				<ul><li>Windows Network Administration</li></ul></li></ul></li> <li>Awarded for Highest Innovation at Missouri S&amp;T Summer Pre-Decision Visit</li>`,z,w,et="Univeristy Education at Mizzou",A,v,it=`On my first day of class at Mizzou, I walked into CS 1050, sat down, and was
+	bewildered by how basic the material was. After the class ended, I immediately
+	contacted my Academic Advisor and asked him about potentially skipping the
+	class for something more advanced. He informed me of a policy that would allow
+	me to skip, and get credit for CS 1050, so long as I could get above a B in
+	the more advanced class, CS 2050. It's now the spring semester, and I have
+	successfully recieved credit for both CS 1050 and 2050 with a cumulative GPA
+	of 3.44 for the fall semester.`,$,x,nt="Related Projects and Hobbies",E,C,at=`I started programing games on <a href="https://scratch.mit.edu">Scratch</a> in
+	elementary school. Inspired to program at an early age, I continued on to create
+	numerous projects on the platform that far exceeded Scratch&#39;s scope. I did all
+	of my programming in Scratch until I moved on in middle school.`,Q,y,lt=`In middle school, I spent a lot of time working with JavaScript and Node.JS.
+	First, I made a basic realtime updating chat application with <a href="https://socket.io/">Socket.IO</a>
+	and <a href="https://expressjs.com/">Express</a>. I contiuned on to make an
+	online multiplayer browser game using the same libraries, hosted on
+	<a href="https://www.heroku.com/">Heroku</a>. At some point during middle
+	school I was also running a small flash games website that was used by many
+	students in the school. I also began hosting a Minecraft server that is still
+	online to this day.`,q,b,st=`In high school, I created a Discord bot via <a href="https://discord.js.org/">Discord.JS</a>. This bot also included a frontend written in
+	<a href="https://react.dev/">React</a> and had persistent functionality that I
+	achieved with <a href="https://www.mongodb.com/">MongoDB</a>. I also wrote
+	HTML proxies from scratch, before I realized there are easier solutions to
+	bypass a blocklist.`,B,I,ot=`I also made the swtich from Windows to Unix like operating systems in high
+	school. I fiddled around with different Linux distrobutions until I settled on <a href="https://archlinux.org/">Arch</a>. I also run some Linux boxes at home, which I use for
+	<a href="https://jellyfin.org/">Jellyfin</a> and as a NAS.`,J,g,rt=`In college, so far I have been working on multiple web applications in <a href="https://kit.svelte.dev/">SvelteKit</a>. These applications have
+	<a href="https://www.sqlite.org/index.html">SQLite</a>
+	and <a href="https://www.postgresql.org/">PostgreSQL</a> databases that I
+	interact with through <a href="https://www.prisma.io/">Prisma</a>. These
+	applications also include authentication functionality that I initally wrote
+	myself; however, I have migrated to
+	<a href="https://lucia-auth.com/">Lucia</a>.`,N,k,ht="Experience as an Employee",O,H,dt="I have been formally employed by buisnesses on two seperate occasions.",U,L,ut=`My first job was working part time at Dairy Queen in the back, which was known
+	as the grill. I prepared all the hot food at that location. My reponsiblietes
+	included managing a grill, multiple fryers, multiple microwaves, two ovens,
+	and the freezer. I always worked the closing shift, so at the end of the
+	night, I would do all the dishes, clean the grill and the fryers, take the
+	grease out back behind the dumpster and pour it into the receptical, clean all
+	surfaces including the insides of the microwaves, take out the trash, and
+	finally mop the floors.`,W,M,pt=`My second job was working full time at Dierbergs in the Deli department. This
+	job was much the same as working at Dairy Queen. Both jobs had lots of hard
+	work preparing food, cleaning, and working the closing shift. The difference
+	with Dierbergs was that I was now working full time, and in a position where I
+	had to both provide customer service, and functionally work as a team with my
+	co-workers, which was in stark contrast to the solitude of Dairy Queen. During
+	my time at Dierbergs, I was also in charge of training new part-time hires.`;return{c(){h=a("h1"),h.textContent=K,S=o(),d=a("p"),d.textContent=G,_=o(),u=a("h2"),u.textContent=V,P=o(),p=a("table"),p.innerHTML=X,T=o(),m=a("h2"),m.textContent=Y,j=o(),c=a("p"),c.textContent=Z,D=o(),f=a("ul"),f.innerHTML=tt,z=o(),w=a("h3"),w.textContent=et,A=o(),v=a("p"),v.textContent=it,$=o(),x=a("h2"),x.textContent=nt,E=o(),C=a("p"),C.innerHTML=at,Q=o(),y=a("p"),y.innerHTML=lt,q=o(),b=a("p"),b.innerHTML=st,B=o(),I=a("p"),I.innerHTML=ot,J=o(),g=a("p"),g.innerHTML=rt,N=o(),k=a("h2"),k.textContent=ht,O=o(),H=a("p"),H.textContent=dt,U=o(),L=a("p"),L.textContent=ut,W=o(),M=a("p"),M.textContent=pt,this.h()},l(t){h=l(t,"H1",{class:!0,"data-svelte-h":!0}),s(h)!=="svelte-11prx9u"&&(h.textContent=K),S=r(t),d=l(t,"P",{"data-svelte-h":!0}),s(d)!=="svelte-uxoyyc"&&(d.textContent=G),_=r(t),u=l(t,"H2",{"data-svelte-h":!0}),s(u)!=="svelte-1bhajqs"&&(u.textContent=V),P=r(t),p=l(t,"TABLE",{"data-svelte-h":!0}),s(p)!=="svelte-1ripjzb"&&(p.innerHTML=X),T=r(t),m=l(t,"H2",{"data-svelte-h":!0}),s(m)!=="svelte-w5efo"&&(m.textContent=Y),j=r(t),c=l(t,"P",{"data-svelte-h":!0}),s(c)!=="svelte-1qeb1sf"&&(c.textContent=Z),D=r(t),f=l(t,"UL",{"data-svelte-h":!0}),s(f)!=="svelte-z6axuh"&&(f.innerHTML=tt),z=r(t),w=l(t,"H3",{"data-svelte-h":!0}),s(w)!=="svelte-a0g9tz"&&(w.textContent=et),A=r(t),v=l(t,"P",{"data-svelte-h":!0}),s(v)!=="svelte-j2xx18"&&(v.textContent=it),$=r(t),x=l(t,"H2",{"data-svelte-h":!0}),s(x)!=="svelte-6un2i0"&&(x.textContent=nt),E=r(t),C=l(t,"P",{"data-svelte-h":!0}),s(C)!=="svelte-c9vgl1"&&(C.innerHTML=at),Q=r(t),y=l(t,"P",{"data-svelte-h":!0}),s(y)!=="svelte-1k1ax1h"&&(y.innerHTML=lt),q=r(t),b=l(t,"P",{"data-svelte-h":!0}),s(b)!=="svelte-19u4u9g"&&(b.innerHTML=st),B=r(t),I=l(t,"P",{"data-svelte-h":!0}),s(I)!=="svelte-9obfpm"&&(I.innerHTML=ot),J=r(t),g=l(t,"P",{"data-svelte-h":!0}),s(g)!=="svelte-kl0s2h"&&(g.innerHTML=rt),N=r(t),k=l(t,"H2",{"data-svelte-h":!0}),s(k)!=="svelte-39jf0x"&&(k.textContent=ht),O=r(t),H=l(t,"P",{"data-svelte-h":!0}),s(H)!=="svelte-11a88a7"&&(H.textContent=dt),U=r(t),L=l(t,"P",{"data-svelte-h":!0}),s(L)!=="svelte-6kutxo"&&(L.textContent=ut),W=r(t),M=l(t,"P",{"data-svelte-h":!0}),s(M)!=="svelte-vlvvee"&&(M.textContent=pt),this.h()},h(){wt(h,"class","font-serif")},m(t,e){i(t,h,e),i(t,S,e),i(t,d,e),i(t,_,e),i(t,u,e),i(t,P,e),i(t,p,e),i(t,T,e),i(t,m,e),i(t,j,e),i(t,c,e),i(t,D,e),i(t,f,e),i(t,z,e),i(t,w,e),i(t,A,e),i(t,v,e),i(t,$,e),i(t,x,e),i(t,E,e),i(t,C,e),i(t,Q,e),i(t,y,e),i(t,q,e),i(t,b,e),i(t,B,e),i(t,I,e),i(t,J,e),i(t,g,e),i(t,N,e),i(t,k,e),i(t,O,e),i(t,H,e),i(t,U,e),i(t,L,e),i(t,W,e),i(t,M,e)},p:R,i:R,o:R,d(t){t&&(n(h),n(S),n(d),n(_),n(u),n(P),n(p),n(T),n(m),n(j),n(c),n(D),n(f),n(z),n(w),n(A),n(v),n($),n(x),n(E),n(C),n(Q),n(y),n(q),n(b),n(B),n(I),n(J),n(g),n(N),n(k),n(O),n(H),n(U),n(L),n(W),n(M))}}}function xt(F,h,K){return[!0]}class bt extends ct{constructor(h){super(),ft(this,h,xt,vt,mt,{prerender:0})}get prerender(){return this.$$.ctx[0]}}export{bt as component};
